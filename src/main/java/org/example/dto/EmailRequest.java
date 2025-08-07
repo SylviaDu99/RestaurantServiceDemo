7c54cad1743bd7d0261bba9ea2b497e65ff10568
SylviaDu99 <dujingyun1999@gmail.com>
2025-08-07 08:07:51 -0700

    local commit

---
+++ src/main/java/org/example/dto/EmailRequest.java
@@ -0,0 +1,16 @@
+package org.example.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class EmailRequest {
+    private String to;
+    private String subject;
+    private String body;
+}
