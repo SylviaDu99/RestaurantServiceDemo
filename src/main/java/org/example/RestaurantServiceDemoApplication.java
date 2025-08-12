12733e9dceaaa0a472202c1d77f46738000326c9
SylviaDu99 <dujingyun1999@gmail.com>
2025-08-12 12:36:01 -0700

    local commit

--- src/main/java/org/example/RestaurantServiceDemoApplication.java
+++ src/main/java/org/example/RestaurantServiceDemoApplication.java
@@ -3,12 +3,14 @@ package org.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
 @EnableFeignClients
+@EnableEurekaClient
 public class RestaurantServiceDemoApplication {
 
     public static void main(String[] args) {
